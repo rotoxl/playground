84b74a7c495078b2b1a705698b8e5af73fb8d551
Ernesto Molina Carrón <rotoxl@gmail.com>
2023-11-09 10:35:52 +0100

    Setup absolute paths and aliases

---
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,22 @@
+import { Language } from '@app/i18n/constants';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Open up HomeScreen.tsx to start working on your app! {Language.EN}
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
